6ed8af13f7dcbea716fe8fe9df2db399fe8986ef
noctrox389 <114263655+noctrox389@users.noreply.github.com>
2023-01-16 20:17:33 -0400

    Add files via upload

---
+++ assets/preload/stages/stagestage.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	-- background 
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		
+	end
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
